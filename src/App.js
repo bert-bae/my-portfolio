35a1fb4367dd99cbb217f9e7be2bc81874f12bde
Elbert Bae <elbert.bae@gmail.com>
2019-03-09 10:18:37 -0800

    general setup of react and react bootstrap

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import { Button, Container, Navbar, Carousel, Jumbotron } from 'react-bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Jumbotron fluid>
+
+          <Navbar expand="lg" variant="light" bg="light">
+            <Navbar.Brand href="#">Navbar</Navbar.Brand>
+          </Navbar>
+
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="http://via.placeholder.com/640x200"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="http://via.placeholder.com/640x200"
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="http://via.placeholder.com/640x200"
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+
+        </Jumbotron>;
+      </div>
+    );
+  }
+}
+
+export default App;
