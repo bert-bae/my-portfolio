c3b779083a0c83f07f6269f05b3c5b04631474c8
Elbert Bae <elbert.bae@gmail.com>
2019-03-10 12:59:24 -0700

    created carousel item template for full height and width of screen

--- src/App.js
+++ src/App.js
@@ -7,49 +7,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Jumbotron fluid>
-
           <Navbar expand="lg" variant="light" bg="light">
             <Navbar.Brand href="#">Navbar</Navbar.Brand>
           </Navbar>
 
-          <Carousel>
+          <Carousel indicators="false">
             <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="http://via.placeholder.com/640x200"
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="http://via.placeholder.com/640x200"
-                alt="Third slide"
-              />
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="http://via.placeholder.com/640x200"
-                alt="Third slide"
-              />
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              </Carousel.Caption>
+              <div class="carousel-item-one">
+                <h1>Hello</h1>
+              </div>
             </Carousel.Item>
           </Carousel>
-
-        </Jumbotron>;
       </div>
     );
   }
