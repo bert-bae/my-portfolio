88d3b9868c4e2c435ee13dfe66f9d77c0e92b1fc
Elbert Bae <elbert.bae@gmail.com>
2019-04-13 11:26:16 -0700

    homepage image containers outlined

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,6 +8,16 @@ export default class Home extends Component {
   render() {
     return (
       <div className="App">
+        <Container className="home-images" fluid={true}>
+          <Container className="outline image-one">
+          </Container>
+          <Container className="outline image-column">
+            <Container className="outline image-two">
+            </Container>
+            <Container className="outline image-three">
+            </Container>
+          </Container>
+        </Container>
         <Container className="section-container center" fluid={true}>
           <Col className="center" lg={4} md={4} sm={4}>
             <a className="profile-icons" href="https://github.com/bert-bae">
