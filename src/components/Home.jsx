41d81ca7fd169edeedde63f62bb603bb40cc1be4
Elbert Bae <elbert.bae@gmail.com>
2019-04-23 18:22:04 -0700

    Created contact popover card

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
-import { Container, Col } from 'react-bootstrap';
-import ContactCard from './ContactCard';
+import { Container, Col, Popover, OverlayTrigger } from 'react-bootstrap';
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      contactCard: false
-    }
   }
   render() {
-    const contactCard = () => {
-      if (this.state.contactCard === true) {
-        this.setState({ contactCard: false });
-      } else {
-        this.setState({ contactCard: true });
-      }
-    }
+    const popover =  (
+      <Popover id="contact-popover" title="Contact Information">
+        <Container className="contact-container">
+          <Container className="section-container container-column">
+            <p className="sectionheader">Personal Email</p>
+            <p className="content-text">elbert.bae@gmail.com</p>
+          </Container>
+          <Container className="section-container container-column">
+            <p className="sectionheader">Work Email</p>
+            <p className="content-text">elbert@colinkz.com</p>
+          </Container>
+          <Container className="section-container container-column">
+            <p className="sectionheader">Phone</p>
+            <p className="content-text">(778)-908-0350</p>
+          </Container>
+        </Container>
+      </Popover>
+    )
     return (
-      <div className="App">
+      <div className="home-container">
         <Container className="home-images" fluid={true}>
           <Container className="outline image-one">
           </Container>
@@ -47,15 +54,17 @@ export default class Home extends Component {
           </a>
           </Col>
           <Col className="center" lg={4} md={4} sm={4}>
-            <span className="fa-stack fa-2x fa-2x profile-icons contact-info" onClick={contactCard}>
-              <i className="circle-rotate fas fa-circle-notch fa-stack-2x"></i>
-              <i className="circle-rotate-slow fas fa-user-circle fa-stack-1x"></i>
-            </span>
+            <OverlayTrigger 
+              trigger="click" 
+              placement="top" 
+              overlay={popover}>
+              <span className="fa-stack fa-2x fa-2x profile-icons contact-info">
+                <i className="circle-rotate fas fa-circle-notch fa-stack-2x"></i>
+                <i className="circle-rotate-slow fas fa-user-circle fa-stack-1x"></i>
+              </span>
+            </OverlayTrigger>
           </Col>
         </Container>
-        {this.state.contactCard && 
-          <ContactCard/>
-        }
       </div>
     )
   } 
