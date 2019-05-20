8ec452e617c999ff8668e965c7a154386d82496b
Elbert Bae <elbert.bae@gmail.com>
2019-05-20 08:52:38 -0700

    created gradient for homepage images

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,7 +4,7 @@ export default function AboutMe() {
   return (
     <div className="about-me-container">
       <div className="highlight-container">
-        <div className="highlight-image" style={{backgroundImage: `url(/images/projects/colinkz1.jpg)`}}></div>
+        <div className="highlight-image odd" style={{backgroundImage: `url(/images/projects/colinkz1.jpg)`}}></div>
         <div className="highlight-content">
           <p className="subheader">About Me</p>
           <p className="content">I am a NodeJS developer with with full-stack knowledge. I have a particular interest in user experience, responsive design, but find it fascinating how data can be analyzed to meet the user and client needs. Having worked in customer service, sales, training, and management in the past, my experience drives me towards creating efficient workflows that improve the experience of organizations and customers.</p>
@@ -16,11 +16,11 @@ export default function AboutMe() {
           <p className="subheader">My Journey</p>
           <p className="content">I too was once a child... And thought coding was magic. My most notable experience prior to development is management with the YMCA's customer service and sales teams. Having lead and worked in a challenging and fulfilling role motivated me to continue learning about the tools that can and will improve the challenges of a chaotic day-to-day operation. With a background in Psychology and Business from Simon Fraser University, I focus on the experience of users and customers on driving business growth and development. Something that is a core driver in my current development role.</p>
         </div>
-        <div className="highlight-image" style={{backgroundImage: `url(/images/resume-jumbo.jpg)`}}></div>
+        <div className="highlight-image even" style={{backgroundImage: `url(/images/resume-jumbo.jpg)`}}></div>
       </div>
 
       <div className="highlight-container">
-        <div className="highlight-image" style={{backgroundImage: `url(/images/projects/colinkz2.jpg)`}}></div>
+        <div className="highlight-image odd" style={{backgroundImage: `url(/images/projects/colinkz2.jpg)`}}></div>
         <div className="highlight-content">
           <p className="subheader">Present Role</p>
           <p className="content">Community, Connection, Culture <br/><br/> As the main software developer for Colinkz Media, I am responsible for the full-stack maintenance and development of our organization's website. Our team works together to create a cultural platform that will be the future gateway used to improving our understanding of diverse cultures and our community. To learn more about the organization:<a href="https://www.colinkz.com/story" alt="Link to Colinkz Media">visit our site!</a>.</p>
