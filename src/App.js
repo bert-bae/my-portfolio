1afa8b43459d59f844745b3f88f2aa70c38d2809
Elbert Bae <elbert.bae@gmail.com>
2019-05-21 00:07:47 -0700

    added browser router as prop

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Header/>
         <Route exact path="/" component={Home}/>
         <Route path="/projects" component={Projects}/>
