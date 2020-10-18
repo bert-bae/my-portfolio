c59afb9e75dfd8eec6ff549f450c3b0acbfd4354
Elbert Bae <elbert.bae@gmail.com>
2020-10-18 16:17:47 -0700

    Updates

---
+++ precache-manifest.1e63d0c8a03428a59d10e03fbdcb5aa7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  },
+  {
+    "revision": "a6b09f34c08cd1785639",
+    "url": "/my-portfolio/static/js/main.bb5fe014.chunk.js"
+  },
+  {
+    "revision": "9ddb19655b05060d2767",
+    "url": "/my-portfolio/static/js/2.cad86985.chunk.js"
+  },
+  {
+    "revision": "a6b09f34c08cd1785639",
+    "url": "/my-portfolio/static/css/main.82ba00aa.chunk.css"
+  },
+  {
+    "revision": "53dde8267456e3eb7a0400e24cc46349",
+    "url": "/my-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.2db940646df4fe28edd1ba40d4350808.js"
+  "/my-portfolio/precache-manifest.1e63d0c8a03428a59d10e03fbdcb5aa7.js"
 );
 
 workbox.clientsClaim();
